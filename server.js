671c4e1beaf244835b7c3f313e5177a5bbf4ad30
Hiteshldt <hitesh.ledoth@gmail.com>
2024-05-12 18:26:43 +0530

    first

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const app = express();
+require('dotenv').config();
 
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000);
 
-// MongoDb Setup
-const db_link = "mongodb+srv://admin:Qbe9pzYeogM7DjlM@cluster0.wuznz5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const db_link = process.env.MongoDb_Link;
+const JWT_KEY = process.env.jwt_key;
 
 mongoose.connect(db_link)
 .then(function(){
@@ -67,12 +69,12 @@ dashboardRoute
 
 authRoute
 .route('/login')
-.get(protectedRoute, getDashboard)
-.post(postLoginPage, protectedRoute, getDashboard);
+.get(protectedRoute2, getLoginPage)
+.post(postLoginPage, getDashboard);
 
 authRoute
 .route('/signup')
-.get(getDashboard)
+.get(protectedRoute2, getSignUpPage)
 .post(postSignUpPage);
 
 authRoute
@@ -99,10 +101,31 @@ function getRecoverPage(req, res) {
 
 function protectedRoute(req, res, next) {
     if(req.cookies.isLoggedIn) {
-        next();
+        const isVerfied = jwt.verify(req.cookies.isLoggedIn, JWT_KEY);
+        if(isVerfied){
+            next();
+        }
+        else{
+            next();
+        }
     }
     else {
         res.redirect('/auth/login');
+    }
+}
+
+function protectedRoute2(req, res, next) {
+    if(req.cookies.isLoggedIn) {
+        const isVerfied = jwt.verify(req.cookies.isLoggedIn, JWT_KEY);
+        if(isVerfied){
+            res.redirect('/dashboard');
+        }
+        else{
+            next();
+        }
+    }
+    else {
+        next();
     }
 }
 
@@ -111,7 +134,15 @@ async function postLoginPage(req, res, next) {
     const user = await signUpData.findOne({ email: req.body.loginEmail });
     if (user && await bcrypt.compare(req.body.loginPassword, user.password)) {
         console.log("RightCred:");
-        res.cookie('isLoggedIn', true, {httpOnly: true});
+
+        let uid = user['.id'];
+        let token = jwt.sign({payload:uid}, JWT_KEY);
+
+        const now = new Date();
+        const expireTime = now.getTime() + 1000 * 60 * 60 * 24 * 7;
+        now.setTime(expireTime);
+        res.cookie('isLoggedIn', token, { httpOnly: true, expires: now, path: '/' });
+
         next();
     } else {
         console.log("wrongCred:");
@@ -126,7 +157,7 @@ async function postSignUpPage(req, res) {
     const hashedPassword = bcrypt.hashSync(req.body.signUpPassword, salt);
 
     try {
-        const signUpUser = await signUpData.create({
+        await signUpData.create({
             name: req.body.signUpName,
             email: req.body.signUpEmail,
             deviceId: req.body.signUpDeviceId, // Store the device ID as plain text
@@ -134,10 +165,11 @@ async function postSignUpPage(req, res) {
             phone: req.body.signUpPhone,
             password: hashedPassword,
         });
-        res.json({status: 'ok'});
-        console.log("success:");
+        res.redirect('/auth/login');
+        console.log("sucess");
     } catch (err) {
-        console.log(err);
-        res.status(500).json({status: 'error', message: 'An error occurred during sign up.'});
+        console.error(err);
+        // Send an error message that will be displayed in a popup on the client side
+        res.render('signup', { message: 'An error occurred during sign up.', error: err });
     }
 }
